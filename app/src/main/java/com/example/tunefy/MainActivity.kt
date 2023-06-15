db310fe00cd57f5dbf53d4b45c74dc2a3ce288f5
JuliusWhite <julianlagogonzalez@gmail.com>
2023-06-15 20:27:12 +0200

    fix: fixed activities bug and make return to main

--- app/src/main/java/com/example/tunefy/MainActivity.kt
+++ app/src/main/java/com/example/tunefy/MainActivity.kt
@@ -15,20 +15,18 @@ class MainActivity : AppCompatActivity() {
         val forYouImage3: ImageView = findViewById(R.id.forYouImage3)
         val forYouImage4: ImageView = findViewById(R.id.forYouImage4)
 
+        val intent = Intent(this, MusicPlayerActivity::class.java)
+
         forYouImage1.setOnClickListener {
-            val intent = Intent(this@MainActivity, MusicPlayerActivity::class.java)
             startActivity(intent)
         }
         forYouImage2.setOnClickListener {
-            val intent = Intent(this@MainActivity, MusicPlayerActivity::class.java)
             startActivity(intent)
         }
         forYouImage3.setOnClickListener {
-            val intent = Intent(this@MainActivity, MusicPlayerActivity::class.java)
             startActivity(intent)
         }
         forYouImage4.setOnClickListener {
-            val intent = Intent(this@MainActivity, MusicPlayerActivity::class.java)
             startActivity(intent)
         }
     }
